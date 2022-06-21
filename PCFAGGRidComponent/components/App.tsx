27b6b55dc69dae84ec8d7fd4a3e61c68380161d5
manoj75 <manoj75@gmail.com>
2022-06-21 16:30:56 +0000

    update

--- PCFAGGRidComponent/components/App.tsx
+++ PCFAGGRidComponent/components/App.tsx
@@ -27,7 +27,7 @@ import { PrimaryButton, DefaultButton } from '@fluentui/react/lib/Button';
 
 import { Panel } from '@fluentui/react/lib/Panel';
 import { useBoolean } from '@fluentui/react-hooks';
-import { StackItem, TextField } from "office-ui-fabric-react";
+import { IStackProps, IStackStyles, Stack, StackItem, TextField } from "office-ui-fabric-react";
 
 
 // Register the required feature modules with the Grid
@@ -49,6 +49,7 @@ function getNodes(request: IServerSideGetRowsRequest, data: any[]) {
             return {
                 group: !!d.children,
                 taskid: d.taskid,
+                guid: d.guid,
                 taskname: d.taskname,
                 apilinestatus: d.apilinestatus,
                 startdate: d.startdate,
@@ -84,7 +85,7 @@ function createServerSideDatasource() {
                 filter = params.request.groupKeys[params.request.groupKeys.length - 1];
             }
 
-            fetch("https://org5a3fbf2f.crm8.dynamics.com/api/data/v9.0/new_projectses?$select=new_taskname,new_percentagecomplete,new_taskid,new_apilinestatus,new_startdate,new_enddate&$filter=new_parenttask eq '" + filter + "'")
+            fetch("https://org5a3fbf2f.crm8.dynamics.com/api/data/v9.0/new_projectses?$select=new_taskname,new_projectsid,new_percentagecomplete,new_taskid,new_apilinestatus,new_startdate,new_enddate&$filter=new_parenttask eq '" + filter + "'")
                 .then((resp) => resp.json())
                 .then((data: any[]) => {
                     console.log("---------------------------");
@@ -168,23 +169,29 @@ function mapCRMColumnsToDetailsListColmns(columnsOnView: any): any {
 
 }
 
+const stackTokens = { childrenGap: 50 };
+const stackStyles: Partial<IStackStyles> = { root: { width: 300 } };
+const columnProps: Partial<IStackProps> = {
+    tokens: { childrenGap: 15 },
+    styles: { root: { width: 300 } },
+};
+
 
 export default function App(context: ComponentFramework.Context<IInputs>) {
     // context.factory.requestRender();
-
-
-    // Dialog start 
+    // Panel open  
     const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] = useBoolean(false);
 
+    const [aplineStatus, setAplineStatus] = useState('');
 
-    // dialog end 
-
+    const gridRef = React.useRef<AgGridReact>(null);
     const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
     const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
 
     const [columnDefs, setcolumnDefs] = useState<ColDef[]>([
         { field: "taskid", hide: true },
         { field: "taskname", hide: true, checkboxSelection: true, },
+        { field: "guid", hide: true },
         { field: "apilinestatus", headerName: 'AP Line Status' },
         { field: "startdate" },
         { field: "enddate" },
@@ -240,7 +247,7 @@ export default function App(context: ComponentFramework.Context<IInputs>) {
         //@ts-ignore
         console.log(Xrm.Utility.getGlobalContext());
 
-        fetch("https://org5a3fbf2f.crm8.dynamics.com/api/data/v9.0/new_projectses?$select=new_taskname,new_percentagecomplete,new_taskid,new_apilinestatus,new_startdate,new_enddate&$filter=new_parenttask eq 'NA'")
+        fetch("https://org5a3fbf2f.crm8.dynamics.com/api/data/v9.0/new_projectses?$select=new_taskname,new_projectsid,new_percentagecomplete,new_taskid,new_apilinestatus,new_startdate,new_enddate&$filter=new_parenttask eq 'NA'")
             .then((resp) => resp.json())
             .then((data: any[]) => {
                 console.log("---------------------------");
@@ -255,80 +262,139 @@ export default function App(context: ComponentFramework.Context<IInputs>) {
             });
     }, []);
 
+
+
+
     useEffect(() => {
         // Update the document title using the browser API
     });
 
-    function BatchPostAccounts() {
-        this.apiUrl = Xrm.Utility.getGlobalContext().getClientUrl() + 
-            "/api/data/v9.1/";
-        this.uniqueId = "batch_" + (new Date().getTime());
-        this.batchItemHeader = "--" + 
-            this.uniqueId + 
-            "\nContent-Type: application/http\nContent-Transfer-Encoding:binary";
-        this.content = [];
+
+
+    function updateEntityOld() {
+        console.log("Update -------------------");
+
+        var data = [];
+        data.push('--batch_123456');
+        data.push('Content-Type: multipart/mixed;boundary=changeset_BBB456');
+        data.push('');
+
+        //first request
+        data.push('--changeset_BBB456');
+        data.push('Content-Type:application/http');
+        data.push('Content-Transfer-Encoding:binary');
+        data.push('Content-ID:1');
+        data.push('');
+        //@ts-ignore
+        data.push('POST ' + Xrm.Page.context.getClientUrl() + '/api/data/v9.0/new_projectses(0be62c2f-7eea-ec11-bb3d-000d3af2a84a) HTTP/1.1');
+        data.push('Content-Type:application/json;type=entry');
+        data.push('');
+        data.push('{ "new_apilinestatus":"account name to updated" }');
+
+
+        //second request
+        data.push('--changeset_BBB456');
+        data.push('Content-Type:application/http');
+        data.push('Content-Transfer-Encoding:binary');
+        data.push('Content-ID:2');
+        data.push('');
+        //@ts-ignore
+        data.push('POST ' + Xrm.Page.context.getClientUrl() + '/api/data/v9.0/new_projectses(262b7247-7eea-ec11-bb3d-000d3af2a84a) HTTP/1.1');
+        data.push('Content-Type:application/json;type=entry');
+        data.push('');
+        data.push('{ "new_apilinestatus":"account name to updated" }');
+        //end of changeset
+
+        data.push('--changeset_BBB456--');
+        //end of batch
+
+        data.push('--batch_123456--');
+
+        var payload = data.join('\r\n');
+        //  var payload = data;
+        console.log(payload);
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'multipart/mixed;boundary=batch_123456',
+                'Accept': 'application/json',
+                'prefer': 'odata.continue-on-error',
+                'Odata-MaxVersion': '4.0',
+                'Odata-Version': '4.0'
+            },
+            body: payload
+            // body: JSON.stringify({
+            //     "@odata.type": "Microsoft.Dynamics.CRM.contact",
+            //     "new_apilinestatus": "Updated",
+            // })
+        };
+
+        console.log(requestOptions);
+        //@ts-ignore
+        fetch(Xrm.Page.context.getClientUrl() + '/api/data/v9.0/$batch', requestOptions)
+            .then(response => response.json())
+            .then(data => { console.log(data) });
+
+
+        // $.ajax(
+        //     {
+        //         method: 'POST',
+        //         url: Xrm.Page.context.getClientUrl() + '/api/data/v8.1/$batch',
+        //         headers: {
+        //             'Content-Type': 'multipart/mixed;boundary=batch_123456',
+        //             'Accept': 'application/json',
+        //             'Odata-MaxVersion': '4.0',
+        //             'Odata-Version': '4.0'
+
+        //         },
+
+        //         data: payload,
+        //         async: false,
+        //         success: function (data, textStatus, xhr) {
+        //             alert("The record has been updated");
+
+        //         },
+        //         error: function (xhr, data, textStatus, errorThrown) {
+
+        //         }
+        //     });
+
+
     }
-        
-    
-    BatchPostAccounts.prototype.addRequestItem = function(entity) {
-        this.content.push(this.batchItemHeader);
-        this.content.push("");
-        this.content.push("POST " + this.apiUrl + "accounts" + " HTTP/1.1");
-        this.content.push("Content-Type: application/json;type=entry");
-        this.content.push("");
-        this.content.push(JSON.stringify(entity));
-    }
-        
-    
-    BatchPostAccounts.prototype.sendRequest = function() {
-        this.content.push("");
-        this.content.push("--" + this.uniqueId + "--");
-        this.content.push(" ");
-    
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", encodeURI(this.apiUrl + "$batch"));
-        xhr.setRequestHeader("Content-Type", "multipart/mixed;boundary=" + 
-            this.uniqueId);
-        xhr.setRequestHeader("Accept", "application/json");
-        xhr.setRequestHeader("OData-MaxVersion", "4.0");
-        xhr.setRequestHeader("OData-Version", "4.0");
-        xhr.addEventListener("load", 
-            function() { 
-                console.log("Batch request response code: " + xhr.status); 
-            });
-        
-        xhr.send(this.content.join("\n"));
-    }
+
 
     function updateEntity() {
-        console.log("Update -------------------");
-        // const requestOptions = {
-        //     method: 'POST',
-        //     headers: { 'Content-Type': 'application/json' },
-        //     body: JSON.stringify({
-        //         "@odata.type": "Microsoft.Dynamics.CRM.contact",
-        //         "new_apilinestatus": "Updated",
-        //     })
-        // };
-        // fetch('https://reqres.in/api/posts', requestOptions)
-        //     .then(response => response.json())
-        //     .then(data => { console.log(data) });
+        console.log(gridRef.current!.api.getSelectedRows());
 
-        var firstAccount = {
-            new_apilinestatus: "Test Account 1"
+        var selRows = gridRef.current!.api.getSelectedRows();
+
+        var guid = selRows[0].guid;
+
+        var data =
+        {
+            "new_apilinestatus": aplineStatus,
         }
+        console.log(data);
 
-        var secondAccount = {
-            new_apilinestatus: "Test Account 2"
-        }
-
-        var batchRequest = new BatchPostAccounts();
-        batchRequest.addRequestItem(firstAccount);
-        batchRequest.addRequestItem(secondAccount);   
-        batchRequest.sendRequest();
-
+        // update the record
+        //@ts-ignore
+        Xrm.WebApi.updateRecord("new_projects", guid, data).then(
+            function success(result: any) {
+                dismissPanel();
+                //@ts-ignore
+                Xrm.Utility.confirmDialog("Record has been updated");
+                gridRef.current!.api.refreshServerSideStore();
+                //     gridRef.current!.api.redrawRows();
+                console.log("Project updated");
+                // perform operations on record update
+            },
+            function (error: any) {
+                console.log(error.message);
+                // handle error conditions
+            }
+        );
     }
-
 
     return (
 
@@ -336,23 +402,25 @@ export default function App(context: ComponentFramework.Context<IInputs>) {
 
             <DefaultButton secondaryText="" onClick={openPanel} text="Update Record(s)" />
 
-            <br />
+            <br /> <br />
             <div style={gridStyle} className="ag-theme-alpine-dark">
 
                 <AgGridReact
+                    ref={gridRef}
                     columnDefs={columnDefs}
                     defaultColDef={defaultColDef}
                     autoGroupColumnDef={autoGroupColumnDef}
                     rowModelType={"serverSide"}
                     serverSideStoreType={"partial"}
                     treeData={true}
-                    rowSelection={"multiple"}
+                    // rowSelection={"multiple"}
                     animateRows={true}
                     isServerSideGroupOpenByDefault={isServerSideGroupOpenByDefault}
                     isServerSideGroup={isServerSideGroup}
                     getServerSideGroupKey={getServerSideGroupKey}
                     onGridReady={onGridReady}
                     groupDefaultExpanded={-1}
+
                 ></AgGridReact>
 
 
@@ -365,15 +433,21 @@ export default function App(context: ComponentFramework.Context<IInputs>) {
                 // You MUST provide this prop! Otherwise screen readers will just say "button" with no label.
                 closeButtonAriaLabel="Close"
             >
-                <StackItem>
-                    <TextField label="AP line Staus" />
-                </StackItem>
-                <StackItem>
-                    <PrimaryButton onClick={updateEntity} text="Save" />
-                    <DefaultButton onClick={dismissPanel} text="Cancel" />
-                </StackItem>
+                <Stack horizontal tokens={stackTokens} styles={stackStyles}>
+                    <Stack {...columnProps}>
+                        <TextField label="AP Line Status" required
+                            onChange={(e) => {
+                                console.log(e);
+                                const v: any = e.target;
+                                setAplineStatus(v.value)
+                            }} />
 
-
+                        <Stack horizontal tokens={stackTokens}>
+                            <PrimaryButton onClick={updateEntity} text="Save" />
+                            <DefaultButton onClick={dismissPanel} text="Cancel" />
+                        </Stack>
+                    </Stack>
+                </Stack>
             </Panel>
 
 
@@ -392,6 +466,7 @@ function createNodes(data: any) {
         dtemp.push({
             "taskname": d[i].new_taskname,
             "taskid": d[i].new_taskid,
+            "guid": d[i].new_projectsid,
             "apilinestatus": d[i].new_apilinestatus,
             "startdate": d[i].new_startdate,
             "enddate": d[i].new_enddate,
@@ -403,4 +478,3 @@ function createNodes(data: any) {
     console.log(dtemp);
     return dtemp;
 }
- 
